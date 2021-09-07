0fa987cf82cf2d9abdeca4af6f5a615f231bd5cb
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2021-09-07 23:41:09 +0530

    Add files via upload

--- Firefox/background.js
+++ Firefox/background.js
@@ -1,4 +1,21 @@
+/*var enabled = true;
+var req = new XMLHttpRequest();
+req.overrideMimeType("application/json");
+req.open('GET', 'https://fusionapps.netlify.app/data.json', true);
+req.onload  = function() {
+   var jsonResponse = JSON.parse(req.responseText);
+   // do something with jsonResponse
+   data_assigner(jsonResponse.blocked_domains);
+};
+req.send(null);
+
+function data_assigner(arr) {
+    blocked_domains = arr; 
+};
+
+var blocked_domains = blocked_domains; */
 var enabled = true;
+var blocked_domains = bg.concat(blocked_domain)
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
 		if(enabled) {
@@ -9,3 +26,21 @@ chrome.webRequest.onBeforeRequest.addListener(
 	{urls: blocked_domains},
 	["blocking"]
 );
+
+/*
+var enabled = true;
+var req = new XMLHttpRequest();
+req.overrideMimeType("application/json");
+req.open('GET', 'https://blockerdns.com/hosts-ads.json', true);
+req.onload  = function() {
+   var jsonResponse = JSON.parse(req.responseText);
+   // do something with jsonResponse
+   data_assigner(jsonResponse.blocked_domains);
+};
+req.send(null);
+
+function data_assigner(arr) {
+    blocked_domains = arr; 
+};
+var blocked_domains = blocked_domains; 
+*/
